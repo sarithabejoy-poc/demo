d24f23b9961a88bb3d434e531e3ff66d69a9fd50
Bejoy <H336812@hbc.com>
2023-03-10 13:09:20 -0500

    first commit

---
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSecurityConfig {
+
+//    @Bean
+//    public Sec filterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.authorizeHttpRequests().anyRequest().permitAll();
+//        return httpSecurity.build();
+//    }
+
+}
